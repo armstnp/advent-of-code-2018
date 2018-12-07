81695b0b32a74be32bd4e9cd08a1c2cbb8f6e12d
Nathan Armstrong <kenogu.labz@gmail.com>
2018-12-06 22:53:12 -0800

    Add JGraphT to the project.

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,7 +1,8 @@
 (defproject advent-of-code-2018 "0.0.1-SNAPSHOT"
   :description "Code for solving the 'Advent of Code 2018' problem set."
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [is-prime "0.1.0"]]
+                 [is-prime "0.1.0"]
+                 [org.jgrapht/jgrapht-core "1.3.0"]]
   :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
   :aot [advent-of-code-2018.core])
 
